9f157a36e3cfbe875d84c569bd17401119794dd2
Paul Morie <pmorie@redhat.com>
2017-05-16 14:53:49 -0400

    Refactor methods into individual files

---
+++ v2/get_catalog.go
@@ -0,0 +1,26 @@
+package v2
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (c *client) GetCatalog() (*CatalogResponse, error) {
+	fullURL := fmt.Sprintf(catalogURL, c.URL)
+
+	response, err := c.prepareAndDoRequest(http.MethodGet, fullURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	switch response.StatusCode {
+	case http.StatusOK:
+		catalogResponse := &CatalogResponse{}
+		if err := c.unmarshalResponse(response, catalogResponse); err != nil {
+			return nil, err
+		}
+		return catalogResponse, nil
+	default:
+		return nil, c.handleFailureResponse(response)
+	}
+}
---
+++ v2/unbind.go
@@ -0,0 +1,34 @@
+package v2
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+func (c *client) Unbind(r *UnbindRequest) (*UnbindResponse, error) {
+	if err := validateUnbindRequest(r); err != nil {
+		return nil, err
+	}
+
+	fullURL := fmt.Sprintf(bindingURLFmt, c.URL, r.InstanceID, r.BindingID)
+	var queryParamBuffer bytes.Buffer
+	appendQueryParam(&queryParamBuffer, serviceIDKey, r.ServiceID)
+	appendQueryParam(&queryParamBuffer, planIDKey, r.PlanID)
+	fullURL += "?" + queryParamBuffer.String()
+
+	response, err := c.prepareAndDoRequest(http.MethodDelete, fullURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	switch response.StatusCode {
+	case http.StatusOK, http.StatusGone:
+		// TODO: should we establish that the response body ('{}') is correct?
+		return &UnbindResponse{}, nil
+	default:
+		return nil, c.handleFailureResponse(response)
+	}
+
+	return nil, nil
+}
