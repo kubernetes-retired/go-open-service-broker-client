59cb2b6868a11f21324e0b246352ec5269dba0dc
Marko Luksa <marko.luksa@gmail.com>
2018-01-24 12:32:11 +0100

    Support service binding response schemas

--- v2/get_catalog_test.go
+++ v2/get_catalog_test.go
@@ -167,7 +167,62 @@ const alphaParameterSchemaCatalogBytes = `{
   }]
 }`
 
-func alphaParameterCatalogResponse() *CatalogResponse {
+const alphaParameterAndResponseSchemaCatalogBytes = `{
+  "services": [{
+    "name": "fake-service",
+    "id": "acb56d7c-XXXX-XXXX-XXXX-feb140a59a66",
+    "description": "fake service",
+    "tags": ["tag1", "tag2"],
+    "requires": ["route_forwarding"],
+    "bindable": true,
+    "bindings_retrievable": true,
+    "metadata": {
+    	"a": "b",
+    	"c": "d"
+    },
+    "dashboard_client": {
+      "id": "398e2f8e-XXXX-XXXX-XXXX-19a71ecbcf64",
+      "secret": "277cabb0-XXXX-XXXX-XXXX-7822c0a90e5d",
+      "redirect_uri": "http://localhost:1234"
+    },
+    "plan_updateable": true,
+    "plans": [{
+      "name": "fake-plan-1",
+      "id": "d3031751-XXXX-XXXX-XXXX-a42377d3320e",
+      "description": "description1",
+      "metadata": {
+      	"b": "c",
+      	"d": "e"
+      },
+      "schemas": {
+      	"service_instance": {
+	  	  "create": {
+	  	  	"parameters": {
+	  		  "foo": "bar"	
+	  	  	}
+	  	  },
+	  	  "update": {
+	  	  	"parameters": {
+	  		  "baz": "zap"
+	  	    }
+	  	  }
+      	},
+      	"service_binding": {
+      	  "create": {
+	  	  	"parameters": {
+      	  	  "zoo": "blu"
+      	    },
+	  	  	"response": {
+      	  	  "qux": "qax"
+      	    }
+      	  }
+      	}
+      }
+    }]
+  }]
+}`
+
+func alphaParameterCatalogResponse(includeResponseSchema bool) *CatalogResponse {
 	catalog := okCatalogResponse()
 	catalog.Services[0].Plans[0].Schemas = &Schemas{
 		ServiceInstance: &ServiceInstanceSchema{
@@ -183,12 +238,20 @@ func alphaParameterCatalogResponse() *CatalogResponse {
 			},
 		},
 		ServiceBinding: &ServiceBindingSchema{
-			Create: &InputParametersSchema{
-				Parameters: map[string]interface{}{
-					"zoo": "blu",
+			Create: &RequestResponseSchema{
+				InputParametersSchema: InputParametersSchema{
+					Parameters: map[string]interface{}{
+						"zoo": "blu",
+					},
 				},
 			},
 		},
+	}
+
+	if includeResponseSchema {
+		catalog.Services[0].Plans[0].Schemas.ServiceBinding.Create.Response = map[string]interface{}{
+			"qux": "qax",
+		}
 	}
 
 	return catalog
@@ -258,7 +321,7 @@ func TestGetCatalog(t *testing.T) {
 				status: http.StatusOK,
 				body:   alphaParameterSchemaCatalogBytes,
 			},
-			expectedResponse: alphaParameterCatalogResponse(),
+			expectedResponse: alphaParameterCatalogResponse(false),
 		},
 		{
 			name:    "schemas not included if API version < 2.13",
@@ -268,6 +331,26 @@ func TestGetCatalog(t *testing.T) {
 				body:   alphaParameterSchemaCatalogBytes,
 			},
 			expectedResponse: okCatalogResponse(),
+		},
+		{
+			name:        "response schemas included if alpha features enabled",
+			version:     Version2_13(),
+			enableAlpha: true,
+			httpReaction: httpReaction{
+				status: http.StatusOK,
+				body:   alphaParameterAndResponseSchemaCatalogBytes,
+			},
+			expectedResponse: alphaParameterCatalogResponse(true),
+		},
+		{
+			name:        "response schemas not included if alpha features disabled",
+			version:     Version2_13(),
+			enableAlpha: false,
+			httpReaction: httpReaction{
+				status: http.StatusOK,
+				body:   alphaParameterAndResponseSchemaCatalogBytes,
+			},
+			expectedResponse: alphaParameterCatalogResponse(false),
 		},
 	}
 
