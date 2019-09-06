66695f7b34b9a3111131dbe292bf9a8bb620812d
Jonathan Berkhahn <jaberkha@us.ibm.com>
2019-09-06 13:34:43 -0700

    change import paths to new repo location

--- v2/fake/fake_test.go
+++ v2/fake/fake_test.go
@@ -5,8 +5,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/pmorie/go-open-service-broker-client/v2"
-	"github.com/pmorie/go-open-service-broker-client/v2/fake"
+	"github.com/kubernetes-sigs/go-open-service-broker-client/v2"
+	"github.com/kubernetes-sigs/go-open-service-broker-client/v2/fake"
 )
 
 func catalogResponse() *v2.CatalogResponse {
